3589e12301fd01fd2b561ea2ca7f60620750efd6
root <root@precise64.(none)>
2016-09-22 09:31:23 +0000

    Change varnish install process

--- varnish/recipes/default.rb
+++ varnish/recipes/default.rb
@@ -14,8 +14,8 @@ execute "install" do
 	command "apt-get install -y gcc-4.9 g++-4.9"
 	command "update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9"
 	command "sudo apt-get install -y apt-transport-https"
-        command "curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -echo 'deb https://repo.varnish-cache.org/debian/ jessie varnish-4.1'\
-    >> /etc/apt/sources.list.d/varnish-cache.list"
+        command "curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -"
+        command "echo 'deb https://repo.varnish-cache.org/debian/ jessie varnish-4.1'\ >> /etc/apt/sources.list.d/varnish-cache.list"
 	command "echo 'deb http://security.ubuntu.com/ubuntu wily-security main' >> /etc/apt/sources.list"
 	command "apt-get update"
 	command "apt-get install -y varnish"
